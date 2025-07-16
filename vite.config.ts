6aaa2fe2ec91224c2ad34064049b806302f7689b
khan <khan>
2025-07-16 15:00:12 +0100

    make relative path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+	base: "./",	
 	plugins: [svelte()],
 	resolve: process.env.VITEST ? { conditions: ['browser'] } : undefined,
 	test: {
